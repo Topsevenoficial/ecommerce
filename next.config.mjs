517341d96763a448f2014e17448fae34c18031c9
Roberto Quispe Tangherlini <admin@topsevenoficial.com>
2025-02-15 14:05:23 -0500

    M20

--- next.config.mjs
+++ next.config.mjs
@@ -2,11 +2,22 @@
 const nextConfig = {
   reactStrictMode: true, // Activar modo estricto de React para detectar posibles problemas
   images: {
-    domains: ["localhost"], // Permitir cargar imágenes desde 'localhost'
-    // Si en producción usas otro dominio, agrégalo aquí, por ejemplo:
-    // domains: ['localhost', 'tu-dominio-en-produccion.com'],
+    // Usamos remotePatterns para permitir imágenes desde rutas específicas
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "1337",
+        pathname: "/uploads/**", // Permite todas las imágenes en la carpeta uploads de tu backend
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/**", // Permite imágenes desde Cloudinary
+      },
+    ],
   },
-  // Opcional: Configuraciones adicionales según tus necesidades
+  // Otras configuraciones adicionales si las necesitas:
   // webpack: (config) => {
   //   // Personaliza la configuración de Webpack aquí si es necesario
   //   return config;
